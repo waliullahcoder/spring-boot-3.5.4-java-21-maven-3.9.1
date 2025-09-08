5e8d7ad764f02558bd2f3eb0060b6d2cb46f9518
waliullahcoder <waliullahbiplob786@gmail.com>
2025-09-08 22:00:06 +0600

    purchase order fetures done

--- accounting-java-react/src/main/java/com/example/common/dto/PurchaseOrderDTO.java
+++ accounting-java-react/src/main/java/com/example/common/dto/PurchaseOrderDTO.java
@@ -1,10 +1,12 @@
 package com.example.common.dto;
-
+import com.fasterxml.jackson.annotation.JsonProperty; // Aita jemon khushi  field name pathano jai form theke
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class PurchaseOrderDTO {
+    @JsonProperty("purchaseOrder")
     private PurchaseOrderInfoDTO purchaseOrder; // nested
+    @JsonProperty("purchaseOrderDetails")
     private List<PurchaseOrderDetailDTO> purchaseOrderDetails;
 }
